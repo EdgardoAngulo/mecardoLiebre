b0a0a7f42efac7d1164738a96ac93eb33b8c97b2
EdgardoAngulo <eangulo@summa-sci.com>
2022-07-04 18:55:20 -0500

    subiendo mercado liebre

---
+++ app.js
@@ -0,0 +1,37 @@
+// Inicio de aplicación M04C02
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// para indicarle a express cual es nuestra carpeta estatica
+const publicPath = path.resolve(__dirname, 'Public');
+app.use( express.static(publicPath));
+
+console.log(publicPath)
+
+// Activamos el servidor en (escuchar)
+app.listen(3090, () => console.log("El servidor 3090 está activo"));
+
+
+// El indice - Menu
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+});
+
+// El indice - Menu
+app.get("/home", (request, response) => {
+    response.sendFile(path.join(__dirname, "/views/home.html"))
+});
+
+// Register
+app.get("/register", (request, response) => {
+    response.sendFile(path.join(__dirname, "/views/register.html"))
+});
+
+
+// login
+app.get("/login", (request, response) => {
+    response.sendFile(path.join(__dirname, "/views/login.html"))
+});
